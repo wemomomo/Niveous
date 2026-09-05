24da52ea737800558ca4dc6e0c14132f1ed24592
Riverside <3884014157@qq.com>
2026-09-06 03:22:24 +0800

    更新 archive.js

--- archive.js
+++ archive.js
@@ -320,7 +320,7 @@
 
       + '<button class="action-trigger-btn" id="generateCardBtn" style="max-width:100%; height:44px; border-radius:12px;" type="button">'
       + '<svg viewBox="0 0 24 24"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>'
-      + '<span> ✥ 铺万镜为卷 ✥ </span>'
+      + '<span> ✥ ⋆ 铺万镜为卷 ⋆ ✥ </span>'
       + '</button>'
       + '</div>'
       + '</div>'
